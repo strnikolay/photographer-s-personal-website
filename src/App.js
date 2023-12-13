db137a1a858671249be8260079333fe0317c727f
Strnikolay <nikolayromanchuk88@gmail.com>
2023-12-13 23:58:23 +0300

    13_12_23

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './components/header';
 import Main from './components/main';
 import Gallery from './components/gallery/gallery';
+import Price from './components/price';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/*<Main/>*/}
-      <Gallery/>
+      {/*<Main/>
+      <Gallery/>*/}
+      <Price />
     </div>
   );
 }
---
+++ src/components/price.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./price.css"
+
+function Price() {
+  return (
+    <div className='services'>
+    <div className='title'>Виды услуг и цена</div>    
+    <div className='service-wrapper'>
+        <div className='service s-one'>
+            <div className='logo'>Фото сессия в студии</div>
+            <div className='desc'>
+            Индивидуальная, семейная, Love Story <br/>
+            4000 руб.\час <br/>
+            выбор студии <br/>стилизация образа; постановка.<br/> Отдельно оплачиваются:  <br/> аренда студии — в соответствии с тарифом студии;  <br/>услуги визажиста — 2500 руб.\полный образ;  <br/>Бонус — видеоролик 1,5 мин.
+            </div>
+        </div>
+        <div className='service s-two'>
+        <div className='logo'>Фото сессия в студии</div>
+            <div className='desc'>
+            Индивидуальная, семейная, Love Story 4000 руб.\час <br/>
+            выбор студии <br/>стилизация образа; постановка.<br/> Отдельно оплачиваются:  <br/> аренда студии — в соответствии с тарифом студии;  <br/>услуги визажиста — 2500 руб.\полный образ;  <br/>Бонус — видеоролик 1,5 мин.
+            </div>
+        </div>
+        <div className='service s-three'>
+        <div className='logo'>Фото сессия в студии</div>
+            <div className='desc'>
+            Индивидуальная, семейная, Love Story <br/>
+            4000 руб.\час <br/>
+            выбор студии <br/>стилизация образа; постановка.<br/> Отдельно оплачиваются:  <br/> аренда студии — в соответствии с тарифом студии;  <br/>услуги визажиста — 2500 руб.\полный образ;  <br/>Бонус — видеоролик 1,5 мин.
+            </div>
+        </div>
+    </div>   
+    </div>
+  )
+}
+
+export default Price
